774d178b31dccf3e481693f024caaa94e96e6700
J6k3School <90271421+J6k3School@users.noreply.github.com>
2022-12-04 21:36:32 -0500

    update

--- group_project/lib/main.dart
+++ group_project/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'Map.dart';
 
@@ -7,17 +8,31 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Location Rating',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Location Rating'),
+    return FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot){
+          if (snapshot.hasError){
+            print("Error initializing Firebase");
+          }
+          if (snapshot.connectionState == ConnectionState.done){
+            print ("Successfully connected to Firebase");
+            return MaterialApp(
+              title: "Cloud Storage",
+              theme: ThemeData(
+                  primarySwatch: Colors.blue
+              ),
+              home: MyHomePage(title: "Cloud Storage"),
+            );
+          }
+          else{
+            return CircularProgressIndicator();
+          }
+        }
     );
   }
 }
