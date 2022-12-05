73aed0adb7510635a5031bfc892cbf5f75709866
J6k3School <90271421+J6k3School@users.noreply.github.com>
2022-12-04 21:20:17 -0500

    update

--- group_project/lib/Map.dart
+++ group_project/lib/Map.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -29,6 +30,7 @@ class MapMainScreen extends State<MyHomePage> {
             ),
             layers: [
 
+
               TileLayerOptions(
                 urlTemplate:
                 'https://api.mapbox.com/styles/v1/j6k3/clac55ccr000z15pepq9uy82q/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiajZrMyIsImEiOiJjbGFjNGI4MmIwNm85M3BvZm56eHBwZHN6In0.p86JDqoVLSTffvlEIpZhNw',
@@ -38,144 +40,157 @@ class MapMainScreen extends State<MyHomePage> {
                 },
               ),
               MarkerLayerOptions(
-                markers: [
-                  for (int i = 0; i < mapMarkers.length; i++)
-                    Marker(
-                      height: 70,
-                      width: 70,
-                      point: mapMarkers[i].location ?? AppConstants.myLocation,
-                      builder: (_) {
-                        return GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                pageController.animateToPage(
-                                  i,
-                                  duration: const Duration(milliseconds: 500),
-                                  curve: Curves.easeInOut,
-                                );
-                                selectedIndex = i;
-                              });
-                            },
-
-                            child: AnimatedScale(
-                              duration: const Duration(milliseconds: 500),
-                              scale: selectedIndex == i ? 1 : 0.75,
-                              child: AnimatedOpacity(
-                                duration: const Duration(milliseconds: 500),
-                                opacity: selectedIndex == i ? 1 : 0.75,
-                                child:Icon(
-                                  Icons.location_pin,
-                                  color: Colors.redAccent,
-                                ),
-                              ),
-                            ),
-                        );
-                      },
-                    ),
-                ],
+                // markers: [
+                //   for (int i = 0; i < mapMarkers.length; i++)
+                //     Marker(
+                //       height: 70,
+                //       width: 70,
+                //       point: mapMarkers[i].location ?? AppConstants.myLocation,
+                //       builder: (_) {
+                //         return GestureDetector(
+                //             onTap: () {
+                //               setState(() {
+                //                 pageController.animateToPage(
+                //                   i,
+                //                   duration: const Duration(milliseconds: 500),
+                //                   curve: Curves.easeInOut,
+                //                 );
+                //                 selectedIndex = i;
+                //               });
+                //             },
+                //
+                //             child: AnimatedScale(
+                //               duration: const Duration(milliseconds: 500),
+                //               scale: selectedIndex == i ? 1 : 0.75,
+                //               child: AnimatedOpacity(
+                //                 duration: const Duration(milliseconds: 500),
+                //                 opacity: selectedIndex == i ? 1 : 0.75,
+                //                 child:Icon(
+                //                   Icons.location_pin,
+                //                   color: Colors.redAccent,
+                //                 ),
+                //               ),
+                //             ),
+                //         );
+                //       },
+                //     ),
+                // ],
               ),
             ],
           ),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 2,
-            height: MediaQuery.of(context).size.height * 0.3,
-            child: PageView.builder(
-              controller: pageController,
-              onPageChanged: (value) {
-                setState(() {
-                  selectedIndex = value;
-                  var currentLocation =
-                      mapMarkers[value].location ?? AppConstants.myLocation;
-                  //_animatedMapMove(currentLocation, 11.5);
-                });
-
-              },
-              itemCount: mapMarkers.length,
-              itemBuilder: (_, index) {
-                final item = mapMarkers[index];
-                return Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Card(
-                    elevation: 5,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    color: const Color.fromARGB(255, 30, 29, 29),
-                    child: Row(
-                      children: [
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Expanded(
-                                child: ListView.builder(
-                                  padding: EdgeInsets.zero,
-                                  scrollDirection: Axis.horizontal,
-                                  itemCount: item.rating,
-                                  itemBuilder:
-                                      (BuildContext context, int index) {
-                                    return const Icon(
-                                      Icons.star,
-                                      color: Colors.orange,
-                                    );
-                                  },
-                                ),
-                              ),
-                              Expanded(
-                                flex: 2,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      item.title ?? '',
-                                      style: const TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold,
-                                        color: Colors.white
-                                      ),
-                                    ),
-                                    const SizedBox(height: 10),
-                                    Text(
-                                      item.address ?? '',
-                                      style: const TextStyle(
-                                        fontSize: 14,
-                                        color: Colors.grey,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(4.0),
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(10),
-                              child: Icon(
-                                Icons.ac_unit,
-                                color: Colors.cyanAccent,
-                                size: 150,),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: 10),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          )
+          // Positioned(
+          //   left: 0,
+          //   right: 0,
+          //   bottom: 2,
+          //   height: MediaQuery.of(context).size.height * 0.3,
+          //   child: PageView.builder(
+          //     controller: pageController,
+          //     onPageChanged: (value) {
+          //       setState(() {
+          //         selectedIndex = value;
+          //         var currentLocation =
+          //             mapMarkers[value].location ?? AppConstants.myLocation;
+          //         //_animatedMapMove(currentLocation, 11.5);
+          //       });
+          //
+          //     },
+          //     itemCount: mapMarkers.length,
+          //     itemBuilder: (_, index) {
+          //       final item = mapMarkers[index];
+          //       return Padding(
+          //         padding: const EdgeInsets.all(15.0),
+          //         child: Card(
+          //           elevation: 5,
+          //           shape: RoundedRectangleBorder(
+          //             borderRadius: BorderRadius.circular(10),
+          //           ),
+          //           color: const Color.fromARGB(255, 30, 29, 29),
+          //           child: Row(
+          //             children: [
+          //               const SizedBox(width: 10),
+          //               Expanded(
+          //                 child: Column(
+          //                   mainAxisAlignment: MainAxisAlignment.center,
+          //                   children: [
+          //                     Expanded(
+          //                       child: ListView.builder(
+          //                         padding: EdgeInsets.zero,
+          //                         scrollDirection: Axis.horizontal,
+          //                         itemCount: item.rating,
+          //                         itemBuilder:
+          //                             (BuildContext context, int index) {
+          //                           return const Icon(
+          //                             Icons.star,
+          //                             color: Colors.orange,
+          //                           );
+          //                         },
+          //                       ),
+          //                     ),
+          //                     Expanded(
+          //                       flex: 2,
+          //                       child: Column(
+          //                         crossAxisAlignment: CrossAxisAlignment.start,
+          //                         children: [
+          //                           Text(
+          //                             item.title ?? '',
+          //                             style: const TextStyle(
+          //                               fontSize: 20,
+          //                               fontWeight: FontWeight.bold,
+          //                               color: Colors.white
+          //                             ),
+          //                           ),
+          //                           const SizedBox(height: 10),
+          //                           Text(
+          //                             item.address ?? '',
+          //                             style: const TextStyle(
+          //                               fontSize: 14,
+          //                               color: Colors.grey,
+          //                             ),
+          //                           ),
+          //                         ],
+          //                       ),
+          //                     ),
+          //                   ],
+          //                 ),
+          //               ),
+          //               const SizedBox(width: 10),
+          //               Expanded(
+          //                 child: Padding(
+          //                   padding: const EdgeInsets.all(4.0),
+          //                   child: ClipRRect(
+          //                     borderRadius: BorderRadius.circular(10),
+          //                     child: Icon(
+          //                       Icons.ac_unit,
+          //                       color: Colors.cyanAccent,
+          //                       size: 150,),
+          //                   ),
+          //                 ),
+          //               ),
+          //               const SizedBox(width: 10),
+          //             ],
+          //           ),
+          //         ),
+          //       );
+          //     },
+          //   ),
+          // )
 
         ],
       ),
     );
+
+  }
+  Future getGrades() async{
+    print("Getting the grades...");
+    print(await FirebaseFirestore.instance.collection('Locations').get());
+    return await FirebaseFirestore.instance.collection('Locations').get();
+  }
+
+  Future _addToDb(data) async{
+    FirebaseFirestore.instance.collection('Locations').doc().set(data);
+    setState(() {
+      print("Added data: $data");
+    });
   }
 
 
--- group_project/lib/MapMarker.dart
+++ group_project/lib/MapMarker.dart
@@ -1,54 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:latlong2/latlong.dart';
 
 class MapMarker {
-  final String? image;
-  final String? title;
-  final String? address;
-  final LatLng? location;
-  final int? rating;
-  // DocumentReference? reference;
+  String? image;
+  String? title;
+  String? address;
+  LatLng? location;
+  int? rating;
+  DocumentReference? reference;
 
-  MapMarker({
-    required this.image,
-    required this.title,
-    required this.address,
-    required this.location,
-    required this.rating,
-  });
+  MapMarker.fromMap(var map, {this.reference}){
+    this.image = map['image'];
+    this.title = map['title'];
+    this.address = map['address'];
+    this.location = map['location'];
+    this.rating = map['rating'];
+  }
 
-  // Product.fromMap(var map, {this.reference}){
-  //   this.image = map['image'];
-  //   this.title = map['title'];
-  //   this.address = map['address'];
-  //   this.location = map['location'];
-  //   this.rating = map['rating'];
-  // }
-  //
-  // Map<String,Object?> toMap(){
-  //   return {
-  //     'image': this.image,
-  //     'title': this.title,
-  //     'address': this.address,
-  //     'location': this.location,
-  //     'rating': this.rating,
-  //   };
-  // }
+  Map<String,Object?> toMap(){
+    return {
+      'image': this.image,
+      'title': this.title,
+      'address': this.address,
+      'location': this.location,
+      'rating': this.rating,
+    };
+  }
 }
 
-final mapMarkers = [
-  MapMarker(
-      image: 'assets/images/restaurant_1.jpg',
-      title: "Osmow's Shawarma",
-      address: '1900 Simcoe St N Unit 103, Oshawa ON L1G4Y3, Canada',
-      location: LatLng(43.9434, -78.8907),
-      rating: 4
-  ),
-  MapMarker(
-      image: 'assets/images/restaurant_1.jpg',
-      title: "Subway",
-      address: '2069 Simcoe St N, Oshawa ON L1H 1G6, Canada',
-      location: LatLng(43.9477,-78.8951),
-      rating: 3
-  ),
-
-];
+// final mapMarkers = [
+//   MapMarker(
+//       image: 'assets/images/restaurant_1.jpg',
+//       title: "Osmow's Shawarma",
+//       address: '1900 Simcoe St N Unit 103, Oshawa ON L1G4Y3, Canada',
+//       location: LatLng(43.9434, -78.8907),
+//       rating: 4
+//   ),
+//   MapMarker(
+//       image: 'assets/images/restaurant_1.jpg',
+//       title: "Subway",
+//       address: '2069 Simcoe St N, Oshawa ON L1H 1G6, Canada',
+//       location: LatLng(43.9477,-78.8951),
+//       rating: 3
+//   ),
+//
+// ];
