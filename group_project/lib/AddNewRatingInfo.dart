edc84ff797167cb5f42df94140da7ff7d09cfbca
J6k3School <90271421+J6k3School@users.noreply.github.com>
2022-12-06 22:52:09 -0500

    general spacing

--- group_project/lib/AddNewRatingInfo.dart
+++ group_project/lib/AddNewRatingInfo.dart
@@ -8,41 +8,44 @@ class AddRating extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Login", style: TextStyle(color: Colors.white),),
+        title: const Text("Star", style: TextStyle(color: Colors.white),),
       ),
       body: Container(
-        padding: const EdgeInsets.all(15),
+        padding: const EdgeInsets.fromLTRB(165, 0, 0, 0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextButton(
+
+            FloatingActionButton(
+
               onPressed: () {
                 Navigator.pop(context, 1);
               },
               child: const Text('1 Star'),
               //go to account creation page using navigator
             ),
-            TextButton(
+            FloatingActionButton(
               onPressed: () {
                 Navigator.pop(context, 2);
               },
               child: const Text('2 Stars'),
               //go to account creation page using navigator
             ),
-            TextButton(
+            FloatingActionButton(
               onPressed: () {
                 Navigator.pop(context, 3);
               },
               child: const Text('3 Stars'),
               //go to account creation page using navigator
             ),
-            TextButton(
+            FloatingActionButton(
               onPressed: () {
                 Navigator.pop(context, 4);
               },
               child: const Text('4 Stars'),
               //go to account creation page using navigator
             ),
-            TextButton(
+            FloatingActionButton(
               onPressed: () {
                 Navigator.pop(context, 5);
               },
